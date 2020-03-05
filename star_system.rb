52557f917708f3a2b537eb088ad41f27a04d3739
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-05 15:06:49 +0000

    planets with no moons

--- star_system.rb
+++ star_system.rb
@@ -6,24 +6,27 @@ class StarSystem
 def initialize (name, planets)
   @name = name
   @planets = planets
-
 end
 
 
-def planet_names
-  return planets.map {|planet| planet.name}
+def planet_names()
+  return @planets.map {|planet| planet.name}
 end
 
 def get_planet_by_name(planet)
-  return planets.find {|planet| planet.name == "Mars"}
+  return @planets.find {|planet| planet.name == "Mars"}
 end
 
 def get_largest_planet()
-  return planets.max {|current_planet, next_planet| current_planet.diameter <=> next_planet.diameter}
+  return @planets.max {|current_planet, next_planet| current_planet.diameter <=> next_planet.diameter}
 end
 
 def get_smallest_planet()
-  return planets.min {|current_planet, next_planet| current_planet.diameter <=> next_planet.diameter}
+  return @planets.min {|current_planet, next_planet| current_planet.diameter <=> next_planet.diameter}
+end
+
+def get_planets_with_no_moons()
+  return @planets.find_all {|planet| planet.number_of_moons == 0}
 end
 
 

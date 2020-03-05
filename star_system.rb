2617e73a918dcda1e60fdc90a4a38416c469742b
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-05 14:33:03 +0000

    initial commit, planets names array test passed

---
+++ star_system.rb
@@ -0,0 +1,37 @@
+require("pry")
+
+class StarSystem
+  attr_reader :name, :planets
+
+def initialize (name , planets)
+  @name = name
+  @planets = planets
+
+end
+
+
+def planet_names
+  p planets.map {|planet| planet.name}
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
